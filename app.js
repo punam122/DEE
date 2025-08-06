43b5326ea88dd3cf58fccd9eb61e9b0d732d9f82
punam122 <124009809+punam122@users.noreply.github.com>
2025-08-06 17:03:52 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let menu=document.querySelector('.mainMenu');      
+let hamburger=document.querySelector('a.mobile');                         
+hamburger.addEventListener('click',function(){
+    
+menu.classList.toggle('show');
+
+})
